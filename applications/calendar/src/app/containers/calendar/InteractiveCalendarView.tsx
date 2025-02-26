d926eb435d9679b7110fb7b64bf55910f1359e65
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-02-26 09:30:04 +0000

    Merge branch 'CALWEB-5626-fix-cancel-button' into 'main'
    Allow event popover to stay open after canceling invite
    
    See merge request web/clients!15754

--- applications/calendar/src/app/containers/calendar/InteractiveCalendarView.tsx
+++ applications/calendar/src/app/containers/calendar/InteractiveCalendarView.tsx
@@ -1467,6 +1467,7 @@ const InteractiveCalendarView = ({
             startEALMetric('edit');
         }
         let hasClosedModalOrPopover = false;
+        let hasThrown = false;
         let hasStartChanged;
         // uid is not defined when we create a new event
         const UID = temporaryEvent.tmpData.uid || TMP_UID;
@@ -1639,6 +1640,7 @@ const InteractiveCalendarView = ({
                 handleCreateNotification(successNotification);
             }
         } catch (e: any) {
+            hasThrown = true;
             if (notificationId) {
                 removeNotification(notificationId);
             }
@@ -1676,7 +1678,7 @@ const InteractiveCalendarView = ({
                 dispatch(eventsActions.markEventsAsSaving({ UID, isSaving: false }));
             }
 
-            if (!hasClosedModalOrPopover) {
+            if (!hasClosedModalOrPopover && !hasThrown) {
                 closeProcessingModal(uniqueId, isModal ? 'modal' : 'popover');
             }
         }
