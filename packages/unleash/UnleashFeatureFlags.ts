d2d7809bedd4905bfe5f60b6335419121b052b66
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-03-05 09:03:13 +0000

    Merge branch 'MAILWEB-6330-fix-scribe-generation-with-selection' into 'main'
    Fix scribe generation refinement when some text is selected
    
    See merge request web/clients!15717

--- packages/unleash/UnleashFeatureFlags.ts
+++ packages/unleash/UnleashFeatureFlags.ts
@@ -128,7 +128,6 @@ enum MailFeatureFlag {
     PasswordNudge = 'PasswordNudge',
     PasswordNudgeForPaidUsers = 'PasswordNudgeForPaidUsers',
     ComposerAssistant = 'ComposerAssistant',
-    ComposerAssistantFormatting = 'ComposerAssistantFormatting',
     WalletRightSidebarLink = 'WalletRightSidebarLink',
     ProtonTips = 'ProtonTips',
     ReplayOnboardingModal = 'ReplayOnboardingModal',
